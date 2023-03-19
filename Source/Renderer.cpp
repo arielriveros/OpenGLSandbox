4f5a0dbbe039d703fd9475fa2edcb88443bdd8b9
Ariel <ariel.riveros@ing.uchile.cl>
2023-03-19 03:21:35 -0300

    feat: Colors in attribute

--- Source/Renderer.cpp
+++ Source/Renderer.cpp
@@ -9,10 +9,10 @@ void Renderer::Init()
 {
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-	float vertices[9] = {
-		-0.5, -0.5, 0.0,
-		 0.0,  0.5, 0.0,
-		 0.5, -0.5, 0.0
+	float vertices[18] = {
+		-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+		 0.0,  0.5, 0.0, 0.0, 1.0, 0.0,
+		 0.5, -0.5, 0.0, 0.0, 0.0, 1.0
 	};
 
 	unsigned int VBO, VAO;
@@ -24,8 +24,13 @@ void Renderer::Init()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Position Attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// Color Attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
