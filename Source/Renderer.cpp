80cbd0ee8a3336b4f7e252ce8980b83caca5d2a4
Ariel <ariel.riveros@ing.uchile.cl>
2023-03-19 04:46:39 -0300

    feat Index buffer with quad

--- Source/Renderer.cpp
+++ Source/Renderer.cpp
@@ -15,18 +15,19 @@ void Renderer::Init()
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
 	float vertices[] = {
+	//	 px	   py	pz   r	  g	   b
 		-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
 		 0.5,  0.5, 0.0, 0.0, 1.0, 0.0,
 		 0.5, -0.5, 0.0, 0.0, 0.0, 1.0,
-		
-		-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-		 0.5,  0.5, 0.0, 0.0, 1.0, 0.0,
 		-0.5,  0.5, 0.0, 1.0, 0.0, 1.0
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int indices[] = {0, 1, 2, 1, 3, 0};
+
+	unsigned int VBO, VAO, IBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &IBO);
 
 	glBindVertexArray(VAO);
 
@@ -41,7 +42,9 @@ void Renderer::Init()
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 }
 
 void Renderer::Render()
@@ -50,10 +53,14 @@ void Renderer::Render()
 
 	m_Program.Bind();
 	
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 }
 
 void Renderer::Shutdown()
 {
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	m_Program.Delete();
 }
