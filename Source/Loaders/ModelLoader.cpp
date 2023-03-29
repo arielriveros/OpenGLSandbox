7387db9bd80a6937d2897b3df14fe8f1ddf7f6d0
Ariel <ariel.riveros@ing.uchile.cl>
2023-03-29 11:07:09 -0300

    perf: Reduced number of draw calls by importing fewer meshes per model

--- Source/Loaders/ModelLoader.cpp
+++ Source/Loaders/ModelLoader.cpp
@@ -16,7 +16,7 @@ void ModelLoader::LoadModel(std::string path, Mesh* target)
 {
     // read file via ASSIMP
     Assimp::Importer importer;
-    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace | aiProcess_RemoveRedundantMaterials | aiProcess_OptimizeGraph | aiProcess_OptimizeMeshes);
     // check for errors
     if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
     {
