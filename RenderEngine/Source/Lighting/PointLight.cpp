559dfd17f3a60a014921b847c31f9c71df3fc585
Ariel <ariel.riveros@ing.uchile.cl>
2023-04-10 23:52:12 -0400

    feat: Edit point lights properties in GUI

--- RenderEngine/Source/Lighting/PointLight.cpp
+++ RenderEngine/Source/Lighting/PointLight.cpp
@@ -14,4 +14,15 @@ PointLight::~PointLight()
 void PointLight::OnGui()
 {
 	Light::OnGui();
+
+	ImGui::PushItemWidth(80);
+	ImGui::DragFloat(("Constant ##" + Name).c_str(), (float*)& Constant, 0.01f, 0.01f);
+
+	ImGui::SameLine(0.0f, 0.0f);
+	ImGui::PushItemWidth(80);
+	ImGui::DragFloat(("Linear ##" + Name).c_str(), (float*)&Linear, 0.01f, 0.001f);
+
+	ImGui::SameLine(0.0f, 0.0f);
+	ImGui::PushItemWidth(80);
+	ImGui::DragFloat(("Quadratic ##" + Name).c_str(), (float*)&Quadratic, 0.01f, 0.0001f);
 }
